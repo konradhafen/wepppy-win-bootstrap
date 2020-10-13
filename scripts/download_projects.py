d4abfc6bb0fc7d017d02944477f30b3923fbed4f
KONRADSPC\khafe <konrad.hafen@gmail.com>
2020-10-13 15:13:53 -0700

    now overwrites if project already exists

--- scripts/download_projects.py
+++ scripts/download_projects.py
@@ -1,5 +1,6 @@
 from download_weppcloud_project import *
 import pandas as pd
+import shutil
 
 
 out_dir = 'E:/konrad/Projects/usgs/hjandrews/wepp/'
@@ -8,4 +9,7 @@ df = pd.read_csv(input_fn)
 
 for i in range(df.shape[0]):
     download_weppcloud_project(df.iloc[i, 2], out_dir)
+    if os.path.isdir(out_dir + df.iloc[i, 3]):
+        shutil.rmtree(out_dir + df.iloc[i, 3])
     os.rename(out_dir + df.iloc[i, 2], out_dir + df.iloc[i, 3])
+    print(i + 1, 'completed')
