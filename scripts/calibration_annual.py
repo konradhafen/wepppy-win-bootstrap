52312fa33497c582310d00b4ee4556f5c25f313e
KONRADSPC\khafe <konrad.hafen@gmail.com>
2020-10-19 13:53:44 -0700

    checking pbias for daily and annual simulations

--- scripts/calibration_annual.py
+++ scripts/calibration_annual.py
@@ -1,12 +1,42 @@
 import argparse
 import pandas as pd
+import numpy as np
 import spotpy
 from spotpy_setup import *
 
 
-def water_year_yield(df):
+def pbias(sim, obs):
+    return 100 * (np.sum(sim-obs) / np.sum(obs))
 
+
+def water_year_yield(df, date_col='DATE', q_col='MEAN_Q'):
+    years = pd.DatetimeIndex(df[date_col]).year.unique()
+    vol = []
+    for i in range(len(years) - 1):
+        start_date = str(years[i]) + '-10-01'
+        end_date = str(years[i+1]) + '-09-30'
+        tmp_dat = df.loc[(df[date_col] >= start_date) & (df[date_col] <= end_date)]
+        vol.append(tmp_dat[q_col].sum() * 15.0 * 60 * 0.0283168)  # convert from cfs to cubic meters
+        # print(years[i], tmp_dat.shape, vol[-1])
+
+    df_wy = pd.DataFrame({'year': years[:-1], 'yield_m3': vol})
     return df_wy
+
+
+def water_year_yield_doy(df, date_col='date', v_col='Qvol'):
+    years = pd.DatetimeIndex(df[date_col]).year.unique()
+    vol = []
+    for i in range(len(years) - 1):
+        start_date = str(years[i]) + '-10-01'
+        end_date = str(years[i+1]) + '-09-30'
+        tmp_dat = df.loc[(df[date_col] >= start_date) & (df[date_col] <= end_date)]
+        # print('number of day in wy:', tmp_dat.shape)
+        vol.append(tmp_dat[v_col].sum())  # convert from cfs to cubic meters
+        # print(years[i], tmp_dat.shape, vol[-1])
+
+    df_wy = pd.DataFrame({'year': years[:-1], 'yield_m3': vol})
+    return df_wy
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -24,16 +54,46 @@ if __name__ == "__main__":
     # ws_code = 'GSWS01'  # watershed of interest
     ws_code = args.site_code
     datetime_format = '%Y-%m-%d'
-    start_year = args.start_year
-    end_year = args.end_year
-    output = wd + '/export/calibration_results_withks.csv'
+    start_year = int(args.start_year)
+    end_year = int(args.end_year)
+    output = wd + '/export/calibration_results_annual.csv'
     # start_date = '2012-01-01'
     # end_date = '2015-12-31'
     df_hist = pd.read_csv(fn_hist)
     df_hist['DATE'] = pd.to_datetime(df_hist['DATE'], format=datetime_format)  # convert dates to date type
     df_hist = df_hist.loc[df_hist['SITECODE'] == ws_code]  # subset to watershed of interest
+    df_hist_q = df_hist.copy()
     df_hist = water_year_yield(df_hist)
 
-    spot_setup = SpotpySetupAnnual(wd, start_year, end_year, df_hist)
-    sampler = spotpy.algorithms.mc(spot_setup, dbname=output, dbformat='csv')
-    sampler.sample(args.reps)
+    fn_wepp = wd + '/wepp/output/chnwb.txt'
+    df_wepp = pd.read_table(fn_wepp, delim_whitespace=True, skiprows=25, header=None)
+    colnames_units = pd.read_table(fn_wepp, delim_whitespace=True, skiprows=21, header=0, nrows=1)
+    df_wepp.columns = colnames_units.columns
+    df_wepp = df_wepp.loc[df_wepp['OFE'] == df_wepp['OFE'].max()]
+    df_wepp['date'] = pd.to_datetime(df_wepp['Y'] * 1000 + df_wepp['J'], format='%Y%j')
+    # print(df_wepp['date'])
+    # df_wepp['date'] = df_wepp['date'].dt.strftime(datetime_format)
+    # print(df_wepp['date'])
+    df_wepp['Qvol'] = (df_wepp['Q'] / 1000.0) * df_wepp['Area']
+    df_wepp['Qday'] = (df_wepp['Qvol'] / (3600 * 24)) / 0.0283168  # cfs
+    print('mean daily Q from WEPP')
+    print(df_wepp['Qday'])
+    df_mod = water_year_yield_doy(df_wepp)
+
+    df_hist = df_hist.loc[(df_hist['year'] >= start_year) & (df_hist['year'] <= end_year)]
+    df_mod = df_mod.loc[(df_mod['year'] >= start_year) & (df_mod['year'] <= end_year)]
+
+    print(df_hist)
+    print(df_mod)
+    print(np.corrcoef(df_hist['yield_m3'].to_numpy(), df_mod['yield_m3'].to_numpy()))
+    print(pbias(df_mod['yield_m3'].to_numpy(), df_hist['yield_m3'].to_numpy()))
+
+    df_hist_q['year'] = pd.DatetimeIndex(df_hist_q['DATE']).year
+    df_hist_q = df_hist_q.loc[(df_hist_q['year'] >= start_year) & (df_hist_q['year'] <= end_year)]
+    df_wepp = df_wepp.loc[(df_wepp['Y'] >= start_year) & (df_wepp['Y'] <= end_year)]
+    print('daily pbias')
+    print(pbias(df_wepp['Qday'].to_numpy(), df_hist_q['MEAN_Q'].to_numpy()))
+
+    # spot_setup = SpotpySetupAnnual(wd, start_year, end_year, df_hist)
+    # sampler = spotpy.algorithms.mc(spot_setup, dbname=output, dbformat='csv')
+    # sampler.sample(args.reps)
