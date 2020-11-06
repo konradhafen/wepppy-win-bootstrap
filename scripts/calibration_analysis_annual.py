b5b194268cfb923bf6f97e20c3eafe258496bd3d
KONRADSPC\khafe <konrad.hafen@gmail.com>
2020-11-06 08:32:23 -0800

    checking results from sensitivity analysis

---
+++ scripts/calibration_analysis_annual.py
@@ -0,0 +1,26 @@
+import numpy as np
+import spotpy
+import os
+import matplotlib.pyplot as plt
+
+wd = "E:/konrad/Projects/usgs/hjandrews/wepp/hja-ws1-base/"
+output = os.path.join(wd, 'export/calibration_results_annual_sens6p.npy')
+nvars = 6
+nrow = 3
+ncol = 2
+xlabs = ['Crop coefficient', 'K (restrictive layer)', 'Deep seepage coefficient', 'Baseflow coefficient', 'Field capacity', 'Percent rock']
+xrange = [(0.8, 1.2), (0.005, 1000), (0.0, 0.2), (0.0, 0.2), (0.0, 0.8), (0, 80)]
+
+data = np.asarray(np.load(output).tolist())
+dat_plt = data[np.where((data[:, 0] > -15.0) & (data[:, 0] < 15.0))]
+
+f, ax = plt.subplots(nrow, ncol, sharex=False)
+
+for i in range(nrow):
+    for j in range(ncol):
+        index = i*ncol+j
+        print(index)
+        ax[i, j].hist(dat_plt[:, index+1], range=xrange[index], bins=40)
+        ax[i, j].set_xlabel(xlabs[index])
+plt.subplots_adjust(hspace=0.3)
+plt.show()
